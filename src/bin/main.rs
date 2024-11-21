c7d9faa5bc865300a2a99f122a0510c27da4cefd
staspv <staspv2008@gmail.com>
2024-11-21 13:28:33 +0300

    конечная реализация сервера

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,10 @@
 use server::get_local_ip;
 use server::ThreadPool;
+use std::io::BufReader;
+use std::time::Duration;
 use std::{
     fs,
+    thread,
     io::prelude::*,
     net::{TcpListener, TcpStream},
 };
@@ -11,26 +14,32 @@ fn main() {
     let listener = TcpListener::bind(format!("{}:7878", local_ip)).unwrap();
     println!("Сервер запущен по адресу: {}:7878",local_ip);
     let pool = ThreadPool::new(4);
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(5) {
         let stream = stream.unwrap();
         pool.execute(|| {
             handle_connection(stream);
         });
     }
+    println!("Сервер остановлен.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
-    stream.read(&mut buffer).unwrap();
-    //println!("{}", String::from_utf8_lossy(&buffer[0..]));
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
     let get = b"GET / HTTP/1.1\r\n";
-    let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" =>{
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
     let content = fs::read_to_string(filename).unwrap();
-    let response = format!("{}{}", status_line, content);
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    let length = content.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{content}");
+    stream.write_all(response.as_bytes()).unwrap();
+    // stream.flush().unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::net::IpAddr;
 
 pub fn get_local_ip() -> Result<IpAddr, std::io::Error>{
     let socket = std::net::UdpSocket::bind("0.0.0.0:0")?;
@@ -8,37 +9,71 @@ pub fn get_local_ip() -> Result<IpAddr, std::io::Error>{
     Ok(local_addr.ip())    
 }
 pub struct ThreadPool{
-    workers: Vec<Worker>,
+    workers :Vec<Worker>,
+    sender  :Option<mpsc::Sender<Job>>,
 }
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool{
     pub fn new(size: usize) -> ThreadPool{
         assert!(size > 0);
+
+        let(sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
         let mut workers = Vec::with_capacity(size);
         for id in 0..size{
-            workers.push(Worker::new(id));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
         ThreadPool{
-            workers
+            workers,
+            sender: Some(sender)
         }
     }
 
     pub fn execute<F>(&self, f: F) where F: FnOnce() + Send + 'static{
+        let job = Box::new(f);
+        self.sender.as_ref().unwrap().send(job).unwrap();
+    }
+}
 
+impl Drop for ThreadPool{
+    fn drop(&mut self) {
+        drop(self.sender.take());
+        for worker in &mut self.workers{
+            println!("Отключение работника {}", worker.id);
+            if let Some(thread) = worker.thread.take(){
+                thread.join().unwrap();
+            }
+        }
     }
 }
 
 struct Worker{
-    id: usize,
-    thread: thread::JoinHandle<()>,
+    id      :usize,
+    thread  :Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker{
-        let thread = thread::spawn(||{});
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker{
+        let thread = thread::spawn(move||loop{
+            let message = receiver.lock().unwrap().recv();
+            match message {
+                Ok(job) =>{
+                    println!("работник {} получил задание. Исполняется", id);
+                    job();
+                }
+                Err(_)=>{
+                    println!("Работник {id} останавливается.");
+                    break;
+                }
+            }
+        });
         Worker{
             id,
-            thread,
+            thread: Some(thread),
         }
     }
 }
+
+
+
